95972c2a9f329ff69d03b21030d990f21f9ef4e6
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-08-10 03:39:11 +0530

    feat: Homepage created

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,31 +4,39 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+// export default function Home() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <Layout
+//       title={`${siteConfig.title}`}
+//       description="Learn JavaScript fundamentals through a comprehensive written guide. Covers variables, data types, functions, scope, arrays, DOM manipulation, and more.">
+//       <HomepageHeader />
+//       <main>
+//         <HomepageFeatures />
+//       </main>
+//     </Layout>
+//   );
+// }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Learn JavaScript fundamentals through a comprehensive written guide. Covers variables, data types, functions, scope, arrays, DOM manipulation, and more.">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+    <Layout>
+		<div style={{ backgroundColor: "#0e0641" }}>
+			{/* set background hero image */}
+
+			<div className={`${styles.hero} hero hero--primary hero--full-height`}>
+				<div className='container' style={{marginInline: "3rem"}}>
+					<div className='row'>
+						<div className='col col--6'>
+							<h1 className={styles.hero__title}>Namaste <span className={styles.span__js}> JavaScript</span> Handbook</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div></Layout>
+	);
 }
