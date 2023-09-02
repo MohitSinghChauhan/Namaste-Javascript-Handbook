fdb9d8dd833b92045efc2f9310747fcf4631d006
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-09-02 13:03:18 +0530

    fix: fixed broken docs  edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,14 +45,14 @@ const config = {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						'https://github.com/MohitSinghChauhan/Namaste-Javascript-Handbook/tree/main/docs',
+						'https://github.com/MohitSinghChauhan/Namaste-Javascript-Handbook/blob/main/',
 				},
 				blog: {
 					showReadingTime: true,
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						'https://github.com/MohitSinghChauhan/Namaste-Javascript-Handbook/tree/main/blog',
+						'https://github.com/MohitSinghChauhan/Namaste-Javascript-Handbook/blob/main/',
 				},
 				theme: {
 					customCss: require.resolve('./src/css/custom.css'),
